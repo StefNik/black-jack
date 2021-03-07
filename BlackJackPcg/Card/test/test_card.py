ef5b402e51fe2b7d6c87d1df054849ef3eb013d0
Stefanos Nikolaou <StefNik>
2021-03-07 11:13:06 +0000

    create test for show()

--- BlackJackPcg/Card/test/test_card.py
+++ BlackJackPcg/Card/test/test_card.py
@@ -3,6 +3,11 @@ from BlackJackPcg.Card import Card
 
 from BlackJackPcg.Utils import CardUtils
 
+import random
+
+import contextlib
+import io
+
 
 class TestCard(TestCase):
 
@@ -62,4 +67,39 @@ class TestCard(TestCase):
         # assert
         self.assertTrue(col in CardUtils.get_possible_colors())
 
+    def test_get_suit_func(self):
+        # arrange
+        c = Card(CardUtils.get_possible_suits()[0], CardUtils.get_possible_colors()[0],
+                 CardUtils.get_possible_numbers()[0])
 
+        # act
+        suit = c.get_suit()
+
+        # assert
+        self.assertTrue(suit in CardUtils.get_possible_suits())
+
+    def test_get_number_func(self):
+        # arrange
+        c = Card(CardUtils.get_possible_suits()[0], CardUtils.get_possible_colors()[0],
+                 CardUtils.get_possible_numbers()[0])
+
+        # act
+        number = c.get_number()
+
+        # assert
+        self.assertTrue(number in CardUtils.get_possible_numbers())
+
+    def test_show_func_list_input(self):
+        # arrange
+        c = Card(
+            random.choice(CardUtils.get_possible_suits()),
+            random.choice(CardUtils.get_possible_colors()),
+            random.choice(CardUtils.get_possible_numbers())
+        )
+
+        with io.StringIO() as buf:
+            # act
+            with contextlib.redirect_stdout(buf):
+                c.show()
+            # assert
+            self.assertTrue(buf.getvalue().strip() == c.to_string())
